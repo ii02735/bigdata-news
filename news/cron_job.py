7c3d85ce50771dabc545f47b7a81e80fe9b37635
Yadallee Bilaal <bilaal.yadallee@insign.fr>
2021-02-05 04:48:54 +0100

    [DOCKER] Configuration changed for mongo connector

--- news/cron_job.py
+++ news/cron_job.py
@@ -4,16 +4,14 @@ import os
 from dotenv import load_dotenv
 from crontab import CronTab
 
+load_dotenv()
+
 with CronTab(user=True) as cron:
 
-    # --packages option : download / add specific package before run spark command
     # cron argument for logging
-
-    job = cron.new(command="""cd /home/jovyan/work/processing && 
-                              /spark/bin/spark-submit --packages 
-                              org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 
-                              --master spark://spark-master:7077 ./script.py cron""")
-    job.minute.every(15)
+    
+    job = cron.new(command="cd /home/jovyan/work/processing && /spark/bin/spark-submit --master spark://spark-master:7077 ./script.py cron")
+    job.minute.every(int(os.getenv("JOB_SCHEDULE")))
     cron.write()
 
 print("Job written !")
